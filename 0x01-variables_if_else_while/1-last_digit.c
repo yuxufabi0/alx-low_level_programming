99d6befc7deb8d7e63834bfdea62e750126b29c7
Yuxufabio <yuxufabio14@yahoo.com>
2023-12-17 08:14:04 +0100

    tiny fix

--- 0x01-variables_if_else_while/1-last_digit.c
+++ 0x01-variables_if_else_while/1-last_digit.c
@@ -1,6 +1,7 @@
 #include <stdlib.h>
 #include <stdio.h>
 #include <time.h>
+
 /**
  * main - prints the last digit of a quotient
  * n: number to check
@@ -15,7 +16,7 @@ int main(void)
 
 	srand(time(0));
 	n = rand() - RAND_MAX / 2;
-	m = n % 10
+	m = n % 10;
 	if (m > 5)
 	{
 	  printf("Last digit of %i is %i and is greater than 5\n", n, m);
