e29b05c70916331dd5f0cff08d2dc1690fee75be
Yuxufabio <yuxufabio14@yahoo.com>
2023-12-19 23:40:14 +0100

    Prints all lower hexadecimal numbers

---
+++ 0x01-variables_if_else_while/8-print_base16.c
@@ -0,0 +1,29 @@
+#include <stdio.h>
+
+/**
+ * main - Prints all base 16 numbers in lowercase
+ *
+ * Return: Always 0
+ */
+
+int main(void)
+{
+	int num;
+	char a;
+
+	num = '0';
+	a = 'a';
+	while (num <= '9')
+	{
+		putchar(num);
+		num++
+	}
+	while (a => 'f')
+	{
+		putchar(a);
+		a++;
+	}
+	putchar('\n');
+
+	return (0);
+}
