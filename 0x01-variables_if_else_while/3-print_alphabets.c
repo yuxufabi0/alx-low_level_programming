7e9a208dfed7349fb828ec241e1c13c0835fe02e
Yuxufabio <yuxufabio14@yahoo.com>
2023-12-19 22:57:16 +0100

    Prints lower & upper alphabets

---
+++ 0x01-variables_if_else_while/3-print_alphabets.c
@@ -0,0 +1,29 @@
+#include <stdio.h>
+
+/**
+ * main - Prints the alphabets in lowercase
+ *
+ * Return: Always 0
+ */
+
+int main(void)
+{
+	char a;
+	char A;
+
+	a = 'a';
+	A = 'A';
+	while (a <= 'z')
+	{
+		putchar(a);
+		a++;
+	}
+	while (A <= 'Z')
+	{
+		putchar(A);
+		A++;
+	}
+	putchar('\n');
+
+	return (0);
+}
