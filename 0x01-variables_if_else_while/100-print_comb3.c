9e4ad7864089d2c8dae721492387f91f34ac5f7a
Yuxufabio <yuxufabio14@yahoo.com>
2023-12-20 00:31:45 +0100

    Prints possilble combo of 2 digits

---
+++ 0x01-variables_if_else_while/100-print_comb3.c
@@ -0,0 +1,33 @@
+#include <stdio.h>
+
+/**
+ * main - Prints all possible different combo
+ * of two digits
+ *
+ * Return: Always 0
+ */
+
+int main(void)
+{
+	int a;
+	int b;
+
+	a = '0';
+	while (a <= '8')
+	{
+		b = a + 1;
+		while (b <= '9')
+		{
+			putchar(a);
+			putchar(b);
+			if (a != '8' || b != '9')
+			{
+				putchar(',');
+				putchar(' ');
+			}
+			b++;
+		}
+		a++;
+	}
+	putchar('\n');
+}
