a153e011615734b974df70ac9ebd3b68f341b6ed
Yuxufabio <yuxufabio14@yahoo.com>
2023-12-19 23:17:08 +0100

    Prints all single digit (base 10)

---
+++ 0x01-variables_if_else_while/6-print_numberz.c
@@ -0,0 +1,22 @@
+#include <stdio.h>
+
+/**
+ * main - Prints all single digits
+ *
+ * Return: Always 0
+ */
+
+int main(void)
+{
+	char num;
+
+	num = '0';
+	while (num <= '9')
+	{
+		putchar(num);
+		num++;
+	}
+	putchar('\n');
+
+	return (0);
+}
