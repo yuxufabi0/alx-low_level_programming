ecc476301ae703392fba903edc67574ce2699775
Yuxufabio <yuxufabio14@yahoo.com>
2023-12-20 15:47:01 +0100

    Prints Jack Bauer minutes

---
+++ 0x02-functions_nested_loops/8-24_hours.c
@@ -0,0 +1,39 @@
+#include "main.h"
+
+/**
+ * jack_bauer - Prints every minute of the day
+ * of Jack Bauer, starting from 00:00 to 23:59
+ *
+ * Return: Always 0
+ */
+
+void	jack_bauer(void);
+
+int main(void)
+{
+    jack_bauer();
+    return (0);
+}
+
+void	jack_bauer(void)
+{
+	int hr;
+	int min;
+
+	hr = 0;
+	while (hr <= 23)
+	{
+		min = 0;
+		while (min <= 59)
+		{
+			_putchar((hr / 10) + '0');
+			_putchar((hr % 10) + '0');
+			_putchar(':');
+			_putchar((min / 10) + '0');
+			_putchar((min % 10) + '0');
+			_putchar('\n');
+			min++;
+		}
+		hr++;
+	}
+}
