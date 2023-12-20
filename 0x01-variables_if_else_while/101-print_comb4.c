719a6f458302251709c5b0776de7f401e6eb2b33
Yuxufabio <yuxufabio14@yahoo.com>
2023-12-20 01:03:15 +0100

    Prints all possible diff combo of 3 digits

---
+++ 0x01-variables_if_else_while/101-print_comb4.c
@@ -0,0 +1,40 @@
+#include <stdio.h>
+
+/**
+ * main - Prints all possible different combo
+ * of three digits
+ *
+ * Return - Always 0
+ */
+
+int main(void)
+{
+	int a;
+	int b;
+	int c;
+
+	a = '0';
+	while (a <= '7')
+	{
+		b = a + 1;
+		while (b <= '8')
+		{
+			c = b + 1;
+			while (c <= '9')
+			{
+				putchar(a);
+				putchar(b);
+				putchar(c);
+				if (a != '9' || b != '8' || c != '7')
+				{
+					putchar(',');
+					putchar(' ');
+				}
+				c++;
+			}
+			b++;
+		}
+		a++;
+	}
+	putchar('\n');
+}
