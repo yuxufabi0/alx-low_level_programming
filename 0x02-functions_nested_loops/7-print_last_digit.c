627fc78a61e1be630f1df883e2549f26f5a1f64e
Yuxufabio <yuxufabio14@yahoo.com>
2023-12-20 10:53:23 +0100

    tiny fix

--- 0x02-functions_nested_loops/7-print_last_digit.c
+++ 0x02-functions_nested_loops/7-print_last_digit.c
@@ -9,18 +9,6 @@
 
 int	print_last_digit(int);
 
-int main(void)
-{
-    int r;
-
-    print_last_digit(98);
-    print_last_digit(0);
-    r = print_last_digit(-1024);
-    _putchar('0' + r);
-    _putchar('\n');
-    return (0);
-}
-
 int	print_last_digit(int num)
 {
 	if (num < 0)
