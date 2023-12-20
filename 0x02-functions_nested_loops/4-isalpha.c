9ef3121a6aec7094072566fea410279f0550da87
Yuxufabio <yuxufabio14@yahoo.com>
2023-12-20 09:23:46 +0100

    quick fix

--- 0x02-functions_nested_loops/4-isalpha.c
+++ 0x02-functions_nested_loops/4-isalpha.c
@@ -7,12 +7,12 @@
  * Return: Always 0
  */
 
-int	_isalpa(int c);
+int	_isalpha(int c);
 
 int	_isalpha(int c)
 {
-	if (c >= 'a' && c <= 'z' ||
-			c >= 'A' && c <= 'Z')
+	if ((c >= 'a' && c <= 'z') ||
+			(c >= 'A' && c <= 'Z'))
 		return (1);
 	return (0);
 }
