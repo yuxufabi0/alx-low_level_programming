51b9cb69b0243df31c28b553d534bdd725615056
Yuxufabio <yuxufabio14@yahoo.com>
2023-12-20 02:38:22 +0100

    update

---
+++ 0x01-variables_if_else_while/102-print_comb5.c
@@ -0,0 +1,21 @@
+#include <stdio.h>
+
+/**
+ * main - Prints all possible combo
+ * of two 2digits numbers
+ *
+ * Return: Always 0
+ */
+
+int main(void)
+{
+	int a;
+	int b;
+
+	a = '0';
+	while (a <= '9')
+	{
+		putchar(a);
+		a++;
+	}
+}
