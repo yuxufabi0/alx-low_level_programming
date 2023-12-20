f096fd54005dc2d468675749f0d863ef1b1e5743
Yuxufabio <yuxufabio14@yahoo.com>
2023-12-20 02:31:45 +0100

    Alphabet func

---
+++ 0x02-functions_nested_loops/1-alphabet.c
@@ -0,0 +1,22 @@
+#include "main.h"
+
+/**
+ * print_alphabet - Prints the lower alphabets
+ *
+ * Return: Always 0
+ */
+
+void print_alphabet(void);
+
+void print_alphabet(void)
+{
+	char a;
+
+	a = 'a';
+	while (a <= 'z')
+	{
+		_putchar(a);
+		a++;
+	}
+	return (0);
+}
