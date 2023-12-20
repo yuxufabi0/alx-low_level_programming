be11dc4a09135833804d476890a56fb9b093e1ac
Yuxufabio <yuxufabio14@yahoo.com>
2023-12-20 09:10:58 +0100

    Checks for alphabetical chars

---
+++ 0x02-functions_nested_loops/4-isalpha.c
@@ -0,0 +1,18 @@
+#include "main.h"
+
+/**
+ * _isalpha - Checks for alpha characters
+ * @c: Takes a character
+ *
+ * Return: Always 0
+ */
+
+int	_isalpa(int c);
+
+int	_isalpha(int c)
+{
+	if (c >= 'a' && c <= 'z' ||
+			c >= 'A' && c <= 'Z')
+		return (1);
+	return (0);
+}
