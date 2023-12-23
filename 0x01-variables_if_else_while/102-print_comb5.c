92ba0766a9cb2c79713834d42b2fc026eff8bd72
Yuxufabio <yuxufabio14@yahoo.com>
2023-12-23 16:03:48 +0100

    Prints all possible combo

--- 0x01-variables_if_else_while/102-print_comb5.c
+++ 0x01-variables_if_else_while/102-print_comb5.c
@@ -7,15 +7,35 @@
  * Return: Always 0
  */
 
+
 int main(void)
 {
 	int a;
 	int b;
 
-	a = '0';
-	while (a <= '9')
+	a = 0;
+	while (a < 100)
 	{
-		putchar(a);
+		b = 0;
+		while (b < 100)
+		{
+			if (a < b)
+			{
+				putchar((a / 10) + 48);
+				putchar((a % 10) + 48);
+				putchar(' ');
+				putchar((b / 10) + 48);
+				putchar((b % 10) + 48);
+				if (a != 98 || b != 99)
+				{
+					putchar(',');
+					putchar(' ');
+				}
+			}
+			b++;
+		}
 		a++;
 	}
+	putchar('\n');
+	return (0);
 }
