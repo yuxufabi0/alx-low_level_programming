553185359cbacdbf1d3d49168ff347f19116abe3
Yuxufabio <yuxufabio14@yahoo.com>
2023-12-27 18:12:37 +0100

    small fix

--- 0x02-functions_nested_loops/102-fibonacci.c
+++ 0x02-functions_nested_loops/102-fibonacci.c
@@ -10,25 +10,25 @@
 
 int main(void)
 {
-    int long old;
-    int long new;
-    int long temp;
-    int i;
+	int long old;
+	int long new;
+	int long temp;
+	int i;
 
-    old = 0;
-    new = 1;
-    i = 0;
-    while (i < 50)
-    {
-        temp = new;
-        new += old;
-        old = temp;
-	printf("%li", new);
-	if (i != 49)
-		printf(", ");
-        i++;
-    }
-    printf("\n");
-    return (0);
+	old = 0;
+	new = 1;
+	i = 0;
+	while (i < 50)
+	{
+		temp = new;
+		new += old;
+		old = temp;
+		printf("%li", new);
+		if (i != 49)
+			printf(", ");
+		i++;
+	}
+	printf("\n");
+	return (0);
 }
 
