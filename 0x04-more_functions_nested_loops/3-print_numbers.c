67c53232c31bea3938b8957fc1152b6be150229b
Yuxufabio <yuxufabio14@yahoo.com>
2023-12-29 09:15:45 +0100

    Prints the numbers 0 to 9

---
+++ 0x04-more_functions_nested_loops/3-print_numbers.c
@@ -0,0 +1,20 @@
+#include "main.h"
+
+/**
+ * print_numbers - Prints the numbers from 0 to 9
+ *
+ * Return: Always 0
+ */
+
+void	print_numbers(void);
+
+void	print_numbers(void)
+{
+	int num;
+
+	num = 0;
+	while (num <= '9')
+	{
+		_putchar(num);
+	}
+}
