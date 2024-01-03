41aafba0daa4d7ee611cb6f3e9a7a55dc7442f6f
Yuxufabio <yuxufabio14@yahoo.com>
2024-01-03 02:45:26 +0100

    Reverses a string

---
+++ 0x05-pointers_arrays_strings/6-puts2.c
@@ -0,0 +1,24 @@
+#include "main.h"
+
+/**
+ * puts2 - Prints every other char of a string, starting with the first char
+ * @str: string to print the chars from
+ */
+void	puts2(char *str)
+{
+	int len, i;
+
+	len = 0;
+
+	while (str[len] != '\0')
+	{
+		len++;
+	}
+
+	for (i = 0; i < len; i += 2)
+	{
+		_putchar(str[i]);
+	}
+
+	_putchar('\n');
+}
