8b7bf00c21d728a2b152a4f7377bb7c2026db56f
Yuxufabio <yuxufabio14@yahoo.com>
2024-01-06 23:34:48 +0100

    Concatenates a string

---
+++ 0x06-pointers_arrays_strings/0-strcat.c
@@ -0,0 +1,16 @@
+#include "main.h"
+
+/**
+ * _strcat - concatenates wo strings
+ * @src: Takes a source string
+ * @dest: Takes a destination string
+ *
+ * Return: Always 0
+ */
+
+char	*_strcat(char *dest, char *src);
+
+char	*_strcat(char *dest, char *src)
+{
+	
+}
