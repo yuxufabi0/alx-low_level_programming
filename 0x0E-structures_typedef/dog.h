980292b30fa74aea1b38f51aa795bc96efdfbce6
Yuxufabio <yuxufabio14@yahoo.com>
2024-01-23 07:00:51 +0000

    Defined a structure dog

--- 0x0E-structures_typedef/dog.h
+++ 0x0E-structures_typedef/dog.h
@@ -1,26 +1,11 @@
 #ifndef DOG_H
 #define DOG_H
 
-/**
- * struct dog - A type with kinda of a dog structure
- * @name: member char for name
- * @age: menber float for age
- * @owner: member char for owner's name
- */
-
-struct dog
+typedef struct
 {
 	char *name;
 	float age;
 	char *owner;
-};
-
-
-void	init_dog(struct dog *d, char *name, float age, char *owner);
-void	print_dog(struct dog *d);
-dog_t	*new_dog(char *name, float age, char *owner);
-void	free_dog(dog_t *d);
-char	*_strcpy(char *dest, char *src);
-int	_strlen(char *s);
+}dog;
 
 #endif
