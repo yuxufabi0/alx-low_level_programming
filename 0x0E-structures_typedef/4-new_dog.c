d09e8a90287b6f6cbcdfaff1a0c7ede4af49225a
yuxufabio <yuxufabio14@yahoo.com>
2024-01-23 16:13:55 +0100

    started creating a function that creates a new dog

---
+++ 0x0E-structures_typedef/4-new_dog.c
@@ -0,0 +1,13 @@
+#include "dog.h"
+
+/**
+ * new_dog - Creates a new dog
+ * @name: Takes a char pointer to name
+ * @age: Takes a float for age
+ * @owner: Takes a char pointer to owner
+ */
+
+dog_t	*new_dog(char *name, float age, char *owner)
+{
+
+}
