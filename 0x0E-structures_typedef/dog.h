09be3ee810c734cada27f1a35bf1515aed1edae2
Yuxufabio <yuxufabio14@yahoo.com>
2024-01-23 07:06:05 +0000

    Defined a structure dog

--- 0x0E-structures_typedef/dog.h
+++ 0x0E-structures_typedef/dog.h
@@ -1,6 +1,13 @@
 #ifndef DOG_H
 #define DOG_H
 
+/**
+ * struct dog - A structure dog
+ * @name: a char member for name
+ * @age: a float member for age
+ * @owner: a char member for owner
+ */
+
 typedef struct
 {
 	char *name;
