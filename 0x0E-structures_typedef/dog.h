9136b1670bd9accfc29551d0c97ed4c34072d8fd
yuxufabio <yuxufabio14@yahoo.com>
2024-01-23 02:49:45 +0100

    dog structure

--- 0x0E-structures_typedef/dog.h
+++ 0x0E-structures_typedef/dog.h
@@ -1,6 +1,18 @@
+#ifndef DOG_H
+#define DOG_H
+
+/**
+ * struct dog - A type with kinda of a dog structure
+ * @name: member char for name
+ * @age: menber float for age
+ * @owner: member char for owner's name
+ */
+
 struct dog
 {
 	char *name;
 	float age;
 	char *owner;
 };
+
+#endif
