576ae04100842aed028834034c9a3565d6373383
Yuxufabio <yuxufabio14@yahoo.com>
2024-02-06 15:43:28 +0100

    creating a dog strucuture

---
+++ 0x0E-structures_typedef/dog.h
@@ -0,0 +1,18 @@
+#ifndef _DOG_
+#define _DOG_
+
+/**
+  * struct dog - A strucrure for a dog
+  * @name: a char pointer
+  * @age: an int
+  * @owner: a char poiner
+  */
+
+struct dog
+{
+	char *name;
+	int age;
+	char *owner;
+};
+
+#endif
