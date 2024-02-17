e11af24ad9cb2b632fcfe47ff065cd1d24ca956a
Yuxufabio <yuxufabio14@yahoo.com>
2024-02-17 13:20:29 +0000

    A program that prints its own name

---
+++ 0x0A-argc_argv/0-whatsmyname.c
@@ -0,0 +1,17 @@
+#include <stdio.h>
+
+/**
+  * main - A program that prints its own name
+  *
+  * Return: Always 0
+  */
+
+int main(int argc, char *argv[])
+{
+	if (argc)
+	{
+		printf("%s\n", argv[0]);
+	}
+
+	return(0);
+}
