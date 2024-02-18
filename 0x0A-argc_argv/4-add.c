8481164f6162a5064399e60b9548952769130b1e
Yuxufabio <yuxufabio14@yahoo.com>
2024-02-18 17:22:23 +0100

    A program that adds its args

---
+++ 0x0A-argc_argv/4-add.c
@@ -0,0 +1,35 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+/**
+  * main - A program that adds all the its args
+  * @argc: an int for arg count
+  * @argv: a char pointer to an array of pointers
+  *
+  * Return: Always 0
+  */
+
+int main(int argc, char *argv[])
+{
+	if (argc <= 1)
+		printf("0\n");
+	else
+	{
+		int i;
+		int sum;
+
+		i = 1;
+		sum = 0;
+		while (i < argc)
+		{
+			if (!atoi(argv[i]))
+			{
+				printf("Error\n");
+				return (1);
+			}
+			sum += atoi(argv[i]);
+			i++;
+		}
+		printf("%d\n", sum);
+	}
+}
